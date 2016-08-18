c9aef440f1f3f459fcb211f70932af108d1351bb
Elizaveta <elizabeth11@bk.ru>
2016-08-18 18:05:42 +0300

    Функция __jsonpCallback объявлена в глобальной области видимости

--- src/pictures.js
+++ src/pictures.js
@@ -2,14 +2,14 @@
 
 var pictures = [];
 
-var __jsonpCallback = function(data) {
+window.__jsonpCallback = function(data) {
   pictures = data;
 };
 
 function createScript(url, callbackFunction) {
   var script = document.createElement('script');
-  script.src = url;
-  document.appendChild('script');
+  script.src = url + '/?callback=' + callbackFunction;
+  document.body.appendChild(script);
 }
 
-createScript('http://localhost:1506/api/pictures?callback=<__jsonpCallback>', __jsonpCallback);
+createScript('http://localhost:1506/api/pictures', '__jsonpCallback');
