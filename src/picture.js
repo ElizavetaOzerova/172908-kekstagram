d0db464d1150feca8a140d282e31e25c28281b4a
Elizaveta <elizabeth11@bk.ru>
2016-08-28 20:08:39 +0300

    Создан модуль с галереей фотографий

--- src/picture.js
+++ src/picture.js
@@ -1,52 +1,58 @@
 'use strict';
 
-(function() {
-  var IMAGE_WIDTH = 182;
-  var IMAGE_HEIGHT = 182;
-  var IMAGE_LOAD_TIMEOUT = 10000;
+// Подключние модуля показа фото в полноэкранном режиме при нажатии на фото из списка.
+var gallery = require('./gallery');
 
-  var templateElement = document.querySelector('#picture-template');
-  var filtersBlock = document.querySelector('.filters');
-  var elementToClone;
+var IMAGE_WIDTH = 182;
+var IMAGE_HEIGHT = 182;
+var IMAGE_LOAD_TIMEOUT = 10000;
 
-  // Прячем блок с фильтрами.
-  filtersBlock.classList.add('hidden');
+var templateElement = document.querySelector('#picture-template');
+var filtersBlock = document.querySelector('.filters');
+var elementToClone;
 
-  if ('content' in templateElement) {
-    elementToClone = templateElement.content.querySelector('.picture');
-  } else {
-    elementToClone = templateElement.querySelector('.picture');
-  }
+// Прячем блок с фильтрами.
+filtersBlock.classList.add('hidden');
 
-  module.exports = function(data, container) {
-    var element = elementToClone.cloneNode(true);
-    element.querySelector('.picture-comments').textContent = data.comments;
-    element.querySelector('.picture-likes').textContent = data.likes;
-    container.appendChild(element);
+if ('content' in templateElement) {
+  elementToClone = templateElement.content.querySelector('.picture');
+} else {
+  elementToClone = templateElement.querySelector('.picture');
+}
 
-    var backgroundImage = new Image();
-    var imgElement = element.querySelector('img');
-    var backgroundLoadTimeout;
+module.exports = function(data, container, indexPicture) {
+  var element = elementToClone.cloneNode(true);
+  element.querySelector('.picture-comments').textContent = data.comments;
+  element.querySelector('.picture-likes').textContent = data.likes;
+  container.appendChild(element);
 
-    backgroundImage.onload = function(event) {
-      clearTimeout(backgroundLoadTimeout);
-      imgElement.src = event.target.src;
-      imgElement.width = IMAGE_WIDTH;
-      imgElement.height = IMAGE_HEIGHT;
-    };
+  var backgroundImage = new Image();
+  var imgElement = element.querySelector('img');
+  var backgroundLoadTimeout;
 
-    backgroundImage.onerror = function() {
-      element.classList.add('picture-load-failure');
-    };
-
-    backgroundLoadTimeout = setTimeout(function() {
-      imgElement.src = '';
-      element.classList.add('picture-load-failure');
-    }, IMAGE_LOAD_TIMEOUT);
-
-    backgroundImage.src = data.url;
-    return element;
+  backgroundImage.onload = function(event) {
+    clearTimeout(backgroundLoadTimeout);
+    imgElement.src = event.target.src;
+    imgElement.width = IMAGE_WIDTH;
+    imgElement.height = IMAGE_HEIGHT;
   };
-  // Отображаем блок с фильтрами.
-  filtersBlock.classList.remove('hidden');
-})();
+
+  backgroundImage.onerror = function() {
+    element.classList.add('picture-load-failure');
+  };
+
+  backgroundLoadTimeout = setTimeout(function() {
+    imgElement.src = '';
+    element.classList.add('picture-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
+
+  // Обработчик клика по блоку с фотографией.
+  element.onclick = function() {
+    gallery.show(indexPicture);
+  };
+
+  backgroundImage.src = data.url;
+  return element;
+};
+// Отображаем блок с фильтрами.
+filtersBlock.classList.remove('hidden');
--- src/pictures.js
+++ src/pictures.js
@@ -4,15 +4,21 @@ var pictures = [];
 var picturesContainer = document.querySelector('.pictures');
 
 // Подключение модуля для работы с данными по сети.
-// Подключение модуля отрисовки одной фотографии.
 var load = require('./load');
+
+// Подключение модуля отрисовки одной фотографии.
 var getPictureElement = require('./picture');
+
+// Подключние модуля показа фото в полноэкранном режиме при нажатии на фото из списка.
+var gallery = require('./gallery');
 
 var callback = function(data) {
   pictures = data;
-  pictures.forEach(function(picture) {
-    getPictureElement(picture, picturesContainer);
+  pictures.forEach(function(picture, indexPicture) {
+    getPictureElement(picture, picturesContainer, indexPicture);
   });
+  // Передача в объект галереи фотографии.
+  gallery.setPictures(pictures);
 };
 
 load('http://localhost:1506/api/pictures', callback);
